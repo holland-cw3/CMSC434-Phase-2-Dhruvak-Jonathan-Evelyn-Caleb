74d80cd74c590e1f8b0bd808c4ec690032c883e0
Jonathan Yim <jonathansyim@gmail.com>
2025-10-23 21:28:48 -0400

    changes to shopping

--- script.js
+++ script.js
@@ -145,3 +145,56 @@ inputField.addEventListener("keydown", (event) => {
     closeKeyboard()
   }
 });
+
+// --- To Do List --- //
+var myNodelist = document.getElementsByTagName("LI");
+var j;
+for (j = 0; j < myNodelist.length; j++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[j].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var j;
+for (j = 0; j < close.length; j++) {
+  close[j].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("todoInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("todoList").appendChild(li);
+  }
+  document.getElementById("todoInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (j = 0; j < close.length; j++) {
+    close[j].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
